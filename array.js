92e5c10b61114782f68d7c85353687cbf1e75a7e
LanPhuongVu07 <69423795+LanPhuong07@users.noreply.github.com>
2020-09-21 21:02:19 +0700

    Update array.js

--- array.js
+++ array.js
@@ -1,3 +1,8 @@
+- cách sử dụng object: object sắp xếp dữ liệu dựa vào các thông tin key: value; 
+	key: value khi truy xuất dữ liệu ta cũng sẽ gọi ra các key.
+- cách sử dụng array: array sắp xếp dữ liệu dựa vào thứ tự các dữ liệu; 
+	khi truy xuất ta sẽ truy cập vào thứ tự các index , bắt đầu từ [0]
+Ví dụ: 
 var arrA = {
   name : 'Phuong',
   age: 20,
