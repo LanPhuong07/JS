e2020065cc9f343f25c31bb689b934fb8d776d4c
LanPhuongVu07 <69423795+LanPhuong07@users.noreply.github.com>
2020-09-21 22:29:10 +0700

    Create b12-3.js

---
+++ b12-3.js
@@ -0,0 +1,6 @@
+var a = '1000';
+var b = '200';
+
+console.log(a > b);
+// Chạy chương trình và tìm hiểu vì sao kết quả lại là false mặc dù 1000 trông có vẻ lớn hơn 200. Viết câu trả lời ở dưới đây.
+// Khi so sánh chuỗi thì giá trị so sánh đầu tiên là ký tự đầu tiên của hai chuỗi. Mà 1 < 2 nên a > b là sai, chương trình trả về giá trị false
